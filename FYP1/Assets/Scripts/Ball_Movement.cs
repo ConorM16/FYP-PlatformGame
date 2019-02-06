d3b575bcab0b3287d0f7c1d5700bbdc1cb95fa75
ConorM16 <muganc@tcd.ie>
2019-02-06 16:20:28 +0000

    Player Key Control Added

---
+++ FYP1/Assets/Scripts/Ball_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Movement : MonoBehaviour
+{
+	private Rigidbody rb;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float moveHor = 2*(Input.GetAxis("Horizontal"));
+		float moveVert = 2*(Input.GetAxis("Vertical"));
+
+		Vector3 movement = new Vector3(moveHor,0.0f,moveVert);
+		rb.AddForce(movement);
+	}
+
+}
