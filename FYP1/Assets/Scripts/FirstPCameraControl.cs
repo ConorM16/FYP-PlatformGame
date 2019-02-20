191235628494374d72bd323afb6e5326c9782f1d
ConorM16 <muganc@tcd.ie>
2019-02-20 18:55:31 +0000

    Camera Rotation Added - Bugged

--- FYP1/Assets/Scripts/FirstPCameraControl.cs
+++ FYP1/Assets/Scripts/FirstPCameraControl.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class FirstPCameraControl : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;
+    protected float fDistance = 1;
+    protected float fSpeed = 1;
+    public GameObject Player;
 
-    void Start()
+    // Update is called once per frame
+    void Update()
     {
-        offset = transform.position - player.transform.position;
+        if (Input.GetKey("d")) OrbitTower(false);
+        if (Input.GetKey("a")) OrbitTower(true);
+        if (Input.GetKey("w")) MoveInOrOut(false);
+        if (Input.GetKey("s")) MoveInOrOut(true);
     }
 
-    void LateUpdate()
+    protected void OrbitTower(bool bLeft)
     {
-        transform.position = player.transform.position + offset;
+        float step = fSpeed * Time.deltaTime;
+        float fOrbitCircumfrance = 2F * fDistance * Mathf.PI;
+        float fDistanceDegrees = (fSpeed / fOrbitCircumfrance) * 360;
+        float fDistanceRadians = (fSpeed / fOrbitCircumfrance) * 2 * Mathf.PI;
+        if (bLeft)
+        {
+            transform.RotateAround(Player.transform.position, Vector3.up, -fDistanceRadians);
+        }
+        else
+            transform.RotateAround(Player.transform.position, Vector3.up, fDistanceRadians);
+    }
+
+    protected void MoveInOrOut(bool bOut)
+    {
+        if (bOut) transform.Translate(0, 0, -fSpeed, Space.Self);
+        else
+            transform.Translate(0, 0, fSpeed, Space.Self);
     }
 }
