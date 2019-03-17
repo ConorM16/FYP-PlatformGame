04f9ec6a7af082c4b95e03594ca84eb74274e20d
ConorM16 <muganc@tcd.ie>
2019-03-17 21:53:23 +0000

    Update Ball_Movement.cs

--- FYP1/Assets/Scripts/Ball_Movement.cs
+++ FYP1/Assets/Scripts/Ball_Movement.cs
@@ -49,9 +49,9 @@ public class Ball_Movement : MonoBehaviour
         SetScoreText();
         setWinText("");
         shadowOn = true;
-        colourEasy = new Vector4(0, 1, 0, 1);
-        colourMed = new Vector4(1, 1, 0, 1);
-        colourHard = new Vector4(1, 0, 0, 1);
+        colourEasy = new Vector4(0, 1, 0, 0.5f);
+        colourMed = new Vector4(0, 0, 1, 0.5f);
+        colourHard = new Vector4(1, 0, 0, 0.5f);
         myCamNew.backgroundColor = colourEasy;
         //        StartCoroutine("changeShadows");
     }
