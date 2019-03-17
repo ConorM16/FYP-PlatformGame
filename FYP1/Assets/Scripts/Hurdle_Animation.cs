1626d921a6ac35ecf19a2edd0041df4f4ee647b2
ConorM16 <muganc@tcd.ie>
2019-03-17 21:22:12 +0000

    Hurdle Animation working

--- FYP1/Assets/Scripts/Hurdle_Animation.cs
+++ FYP1/Assets/Scripts/Hurdle_Animation.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Hurdle_Animation : MonoBehaviour
 {
     //adjust this to change speed
-    float speed = 5f;
+    float speed = 2f;
     //adjust this to change how high it goes
     float height = 0.5f;
+
+    float firstY;
+
+    void Start()
+    {
+        firstY = transform.position.y;
+    }
 
     void Update()
     {
         //get the objects current position and put it in a variable so we can access it later with less code
         Vector3 pos = transform.position;
+        //Debug.Log(pos);
         //calculate what the new Y position will be
         float newY = Mathf.Sin(Time.time * speed);
+        newY = newY * height + firstY;
         //set the object's Y to the new calculated Y
-        transform.position = new Vector3(pos.x, pos.y, pos.z) * height;
+        transform.position = new Vector3(pos.x, newY, pos.z);
     }
 }
